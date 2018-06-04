67e613194a17bddf11bee75aeed8ae9ad45fadb0
LEEJIHYUN <dlwlgus4553@gmail.com>
2018-06-04 11:15:03 +0900

    11시14분

--- src/tictactoe/InputHandle.java
+++ src/tictactoe/InputHandle.java
@@ -1,51 +1,41 @@
 package tictactoe;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class InputHandle {
-   //char 배열이 아니라 int 배열이라 파라미터 수장하겠습니당!
-	//static 굳이 안써도 될거같아요
-  /* public int run(int[][] map, int num) {
-      
-      Scanner kb = new Scanner(System.in);
-      int number;
-      
-      do {
-         System.out.println("<<Input number>>");
-         number = kb.nextInt();
-         //close추가했습니당.
-         kb.close();
-      }while(check(map, number) == false);
-      kb.close();
-      return number;
-      
-   }*/
    
    public static void run(int[][] map) {
-	      
-	      Scanner kb = new Scanner(System.in);
+         
+         Scanner kb = new Scanner(System.in);
 
-	      int number;
-	      
-	      do {
-	         System.out.println("<<Input number>>");
-	         number = kb.nextInt();
-	        
-	         //close추가했습니당.
-	      
-	      }while(check(map, number) == false);
-	   
-	    return;
-	      
-	   }
-   //int배열로 수정하겠습니당
-   //static 굳이 안써도 될거같아요
+         int number;
+         
+         try {
+            do {
+                System.out.println("<<Input number>>");
+                number = kb.nextInt(); 
+                
+             }while(check(map, number) == false);
+         }
+         
+         catch(InputMismatchException e) {
+              System.out.println("문자를 입력받았습니다.");
+              run(map);
+            }
+         
+         return;
+      }
+
    private static boolean check (int[][] map, int num) {
 
       int row ;
       int column ;
       
       switch(num) {
+      case 0:
+         System.out.println("1~9사이의 정수만 입력해주세요~");
+         return false;
       case 1:
          row = 0;
          column = 0;
@@ -91,24 +81,21 @@ public class InputHandle {
          System.out.println(num + ": NO Number!!");
          return false;
       }else {
-    	  if(1==map[row][column]) {
-    		  System.out.println("It's already exist-user");
-    		  return false;
-    	  }
-    	  else if(-1==map[row][column]) {
-    		  System.out.println("It's already exist-com");
-    		  return false;
-    	  }
-    	  else {
-    		  map[row][column]=1;
-    		  return true;
-    	  }
-    		 
-  
-         
+         if(1 == map[row][column]) {
+            System.out.println("It's already exist-user");
+            return false;
+         }
+         else if(-1 == map[row][column]) {
+            System.out.println("It's already exist-com");
+            return false;
+         }
+         else {
+            map[row][column]=1;
+            return true;
+         }
       }
+      
+ 
    }
    
-
-
 }
